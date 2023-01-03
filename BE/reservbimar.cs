25b10435023a5da92eeb0ef4f6be43b0e939ce91
alijafari0572 <alijafari0572@gmail.com>
2023-01-03 22:53:23 +0330

    Create reservbimar.cs

---
+++ BE/reservbimar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class reservbimar
+    {
+        public int id { get; set; }
+        public int codreserv { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+        public List<bimar> bimsrs { get; set; } = new List<bimar>();
+    }
+}
