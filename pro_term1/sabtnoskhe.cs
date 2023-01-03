4feff462c32a3ecdce020a7293ac9f1c73ec6ba3
alijafari0572 <alijafari0572@gmail.com>
2023-01-03 22:52:51 +0330

    Create sabtnoskhe.cs

---
+++ pro_term1/sabtnoskhe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pro_term1
+{
+    public partial class sabtnoskhe : Form
+    {
+        public sabtnoskhe()
+        {
+            InitializeComponent();
+        }
+
+        private void sabtnoskhe_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
