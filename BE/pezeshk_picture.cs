eb97ba5e84195dd31a59af21d20b8ab9aa31a09d
alijafari0572 <alijafari0572@gmail.com>
2023-01-03 22:53:43 +0330

    Create pezeshk_picture.cs

---
+++ BE/pezeshk_picture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class pezeshk_picture
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string codsabtpezeshk { get; set; }
+        public string pictureAddress { get; set; }
+    }
+}
