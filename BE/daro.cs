479646f48881b11ffe41f6b7bb7724263f2c2eee
alijafari0572 <alijafari0572@gmail.com>
2023-01-03 22:53:30 +0330

    Create daro.cs

---
+++ BE/daro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class daro
+    {
+        public int id { get; set; }
+        public string farname { get; set; }
+        public string Engname { get; set; }
+        public string generic { get; set; }
+    }
+}
