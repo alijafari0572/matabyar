ec070165f6891f9bbef938833b6da46f48850e2f
alijafari0572 <alijafari0572@gmail.com>
2023-01-03 22:53:38 +0330

    Create bime.cs

---
+++ BE/bime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class bime
+    {
+        public int id { get; set; }
+        public int codbime { get; set; }
+        public int codbime_marcaz { get; set; }
+        public string name { get; set; }
+        public string what { get; set; }
+        public int tarefeomomi { get; set; }
+        public int tarefemotakhses { get; set; }
+    }
+}
